85ed6e3dad7dde68668cc517ef0f6107786bd4ad
MikeTarkington <tarkmike@gmail.com>
2017-07-31 16:54:19 -0700

    reverted back changes in attempt to fix asset pipeline issue failing to load image over heroku

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,5 @@ module DigiCiti
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # added per https://stackoverflow.com/questions/19650621/heroku-upload-precompiling-assets-failed to fix image not loading over heroku
-    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true #false by default
-  config.assets.digest = true #this line wasn't included by default
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
