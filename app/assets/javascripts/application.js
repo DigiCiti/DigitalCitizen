57afbd5913b023784305713c283801e2b4f5bbf6
MikeTarkington <tarkmike@gmail.com>
2017-08-01 14:33:31 -0700

    checking initial impact of imported styles from us standards guide

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,33 +14,39 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
-  memberSearchListener();
-});
-
-var memberSearchListener = function () {
-  $("#member-search").on('submit', function (e) {
-    e.preventDefault();
-
-    var $memberSearchForm = $(this);
-    var url = $(this).attr('action');
-    var method = $(this).attr('method');
-    var data = $(this).serialize();
-
-    $.ajax({
-      url: url,
-      method: method,
-      data: data
-    })
-    // console.log(data);
-    .done(function(response) {
-      $('#search-result-holder').html(response);
-      console.log("success");
-    })
-    .fail(function(r) {
-      console.log("error ajaxing in the politician", r);
-    });
 
 
-  })
-}
+
+
+// ported js from sinatra version of app... it handles ajax of member search result
+// but is probably not directly useful in it's current state:
+// $(document).ready(function() {
+//   memberSearchListener();
+// });
+//
+// var memberSearchListener = function () {
+//   $("#member-search").on('submit', function (e) {
+//     e.preventDefault();
+//
+//     var $memberSearchForm = $(this);
+//     var url = $(this).attr('action');
+//     var method = $(this).attr('method');
+//     var data = $(this).serialize();
+//
+//     $.ajax({
+//       url: url,
+//       method: method,
+//       data: data
+//     })
+//     // console.log(data);
+//     .done(function(response) {
+//       $('#search-result-holder').html(response);
+//       console.log("success");
+//     })
+//     .fail(function(r) {
+//       console.log("error ajaxing in the politician", r);
+//     });
+//
+//
+//   })
+// }
