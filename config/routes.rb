fe359749ba48c11d689d08593884b08539d6c3fe
MikeTarkington <tarkmike@gmail.com>
2017-08-02 14:31:48 -0700

    clean up initial styling implementation for us design standards in prep for an update to heroku app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :quiz_responses, only: [:new, :create, :show]
   resources :congress_members, only: [:show]
