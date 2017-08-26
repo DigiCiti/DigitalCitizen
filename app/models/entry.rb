37754bc1a877cea4432bf0ad7bd7af6cefdf1269
MikeTarkington <tarkmike@gmail.com>
2017-08-25 20:08:01 -0700

    Add a few primer polymorphic associations for comments and votes/likes. Also setup one to many assocaition for users having many posts.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,5 @@
 class Entry < ApplicationRecord
-  
+  belongs_to :user
+  has_many :comments, as: :commentable
+  has_many :votes, as: :votable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :friendships
-
+  has_many :entries
   has_many :responses
   has_many :quizzes, through: :responses, source: :quiz_id
 
