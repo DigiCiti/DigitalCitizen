3ff531a5f3553f53bee4ffcd536fb52f614d4915
MikeTarkington <tarkmike@gmail.com>
2017-09-25 20:40:25 -0700

    Complete tangent and finish ajaxification of post creation on ones own user profile page. With smooth transitions to reveal/hide the button and form, while also appending the post without page loads.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,20 +8,21 @@ class EntriesController < ApplicationController
   end
 
   def create
-    entry = Entry.new
-    entry.user_id = current_user.id
-    entry.title = params[:title]
-    entry.body = params[:body]
-    entry.entry_type = params[:entry_type]
-    if entry.body == ""
+    @entry = Entry.new
+    @entry.user_id = current_user.id
+    @entry.title = params[:title]
+    @entry.body = params[:body]
+    @entry.entry_type = params[:entry_type]
+    if @entry.body == ""
       flash[:error] = "Post body must not be blank."
     else
-      if entry.save
+      if @entry.save
         flash[:notice] = "Your post was saved!"
       else
-        flash[:error] = entry.errors.full_messages
+        flash[:error] = @entry.errors.full_messages
       end
     end
+    @user = current_user
     respond_to do |format|
       format.html { redirect_to controller: 'users', action: 'show', id: current_user.id }
       format.js
